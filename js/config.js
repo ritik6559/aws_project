802f1fd4071d158a8fa373c9c6f9b51444bb22ff
kritikapandey1 <146093122+kritikapandey1@users.noreply.github.com>
2025-11-19 22:35:11 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_PLPjM4IGG', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6knl2dhqp5d32nf7hqapce3lgr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6n9q6c04q3snb2rlqhjdu74vru', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
